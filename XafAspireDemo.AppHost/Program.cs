756076d98175bed0bd75bc5ed7c6116dd847b8d7
Oli Sturm <oliver@oliversturm.com>
2025-03-20 10:06:56 +0000

    https endpoint added manually

--- XafAspireDemo.AppHost/Program.cs
+++ XafAspireDemo.AppHost/Program.cs
@@ -1,5 +1,7 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddProject<Projects.XafAspireDemo_Blazor_Server>("xafaspiredemo-blazor-server");
+builder
+    .AddProject<Projects.XafAspireDemo_Blazor_Server>("xafaspiredemo-blazor-server")
+    .WithHttpsEndpoint();
 
 builder.Build().Run();
